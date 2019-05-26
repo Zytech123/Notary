7422c3647ba749cdea5dbcadd51391de7a414b68
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-05-26 15:08:54 +0300

    init

--- src/App.svelte
+++ src/App.svelte
@@ -34,23 +34,45 @@
 
 <style>
   .container {
-    margin: 1rem;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    height: 100%;
   }
-  .actions {
-    width: 100%;
-    max-width: 400px;
+  article {
+    flex-basis: 100%;
+    flex: 1;
+  }
+  .right {
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden
   }
-  .actions button {
-    width: 49%;
+  .right img {
+    transform: translateZ(0);
+    flex-shrink: 0;
+    min-height: 100%
+  }
+  @media (max-width: 640px) {
+    .right {
+      display: none !important;
+    }
+  }
+  @media (min-width: 1440px) {
+    .right img {
+      min-width: 100%;
+    }
   }
 </style>
 
 <div class="container">
-  <h1>Orbs Notary</h1>
+  <article class="left">
+    <h1>Orbs Notary</h1>
+  </article>
+  <article class="right">
+    <img src="images/planet-min.png" alt="planet"/>
+  </article>
+  <!-- <h1>Orbs Notary</h1>
   <Explanations />
   <Input
     on:change={ev => {
@@ -65,5 +87,5 @@
   {/if}
   {#if results}
     <Result result={results} />
-  {/if}
+  {/if} -->
 </div>
